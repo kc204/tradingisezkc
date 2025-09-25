2f19fcea3fd75bdff639a8dafedae417371a4f70
Kacey Nwadike <kaceynwadike@gmail.com>
2025-09-25 20:18:38 +0000

    Try fixing this error: `Runtime Error: Error: ./src/app An issue occurre

--- src/app/crypto/sitemap/page.tsx
+++
@@ -1,2 +0,0 @@
-// This file is being removed to resolve the route conflict.
-// Its contents have been moved to /app/crypto/site-map/page.tsx.
--- src/app/sitemap/page.tsx
+++
@@ -1,2 +0,0 @@
-// This file is being removed to resolve the route conflict.
-// Its contents have been moved to /app/site-map/page.tsx.
